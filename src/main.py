d18762241c3f06b96db60abdd3e180b82c6642cd
Brett Jia <dev.bjia56@gmail.com>
2024-08-01 00:31:59 -0400

    use new builds with new option added to main menu

--- src/main.py
+++ src/main.py
@@ -64,30 +64,30 @@ DOWNLOADS = {
     },
     "linux": {
         "x86_64": {
-            "url": "https://github.com/bjia56/btop-builder/releases/download/v1.3.2-2/btop-linux-x86_64.zip",
+            "url": "https://github.com/bjia56/btop-builder/releases/download/v1.3.2-3/btop-linux-x86_64.zip",
             "exe": "btop/bin/btop",
             "extract": extract_zip,
         },
         "aarch64": {
-            "url": "https://github.com/bjia56/btop-builder/releases/download/v1.3.2-2/btop-linux-aarch64.zip",
+            "url": "https://github.com/bjia56/btop-builder/releases/download/v1.3.2-3/btop-linux-aarch64.zip",
             "exe": "btop/bin/btop",
             "extract": extract_zip,
         },
     },
     "darwin": {
         "x86_64": {
-            "url": "https://github.com/bjia56/btop-builder/releases/download/v1.3.2-2/btop-darwin-universal.zip",
+            "url": "https://github.com/bjia56/btop-builder/releases/download/v1.3.2-3/btop-darwin-universal.zip",
             "exe": "btop/bin/btop",
             "extract": extract_zip,
         },
         "arm64": {
-            "url": "https://github.com/bjia56/btop-builder/releases/download/v1.3.2-2/btop-darwin-universal.zip",
+            "url": "https://github.com/bjia56/btop-builder/releases/download/v1.3.2-3/btop-darwin-universal.zip",
             "exe": "btop/bin/btop",
             "extract": extract_zip,
         },
     },
 }
-DOWNLOAD_CACHE_BUST = "20240731-2"
+DOWNLOAD_CACHE_BUST = "20240801-0"
 
 
 class BtopPlugin(ScryptedDeviceBase, StreamService, DeviceProvider, Settings):
